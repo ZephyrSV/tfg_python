fadb377c65d55f597babd7ec3e81ec4ba665eaf5
ZephyrSV <zserret@me.com>
2023-07-10 12:25:31 +0200

    got the reactions nicely listed

--- human_pathways.py
+++ human_pathways.py
@@ -1,5 +1,9 @@
 import sys
 from Bio.KEGG import REST
+from Bio.KEGG.KGML import KGML_parser
+
+def lmap(f, l):
+    return list(map(f,l))
 
 human_pathways = REST.kegg_list("pathway", "hsa").read().split("\n")
 human_pathways = map(lambda x: x.split("\t"), human_pathways)
@@ -16,8 +20,14 @@ else:
     human_pathways = human_pathways[0:1]
     print(f"Using the first pathway {human_pathways[0]['entry']}")
 
-pathway_file = map(lambda x: REST.kegg_get(x['entry'], 'kgml').read(), human_pathways)
+kgmls = map(lambda x: REST.kegg_get(x['entry'], 'kgml').read(), human_pathways)
+#pathway_file = list(map(lambda x: Map.parse(REST.kegg_get(x['entry']).read()), human_pathways))
 
-for e in pathway_file:
-    print(e)
-    
+pathways = list(lmap(lambda x : KGML_parser.parse(x), kgmls)[0])
+pathway_reactions = lmap(lambda x : x.reactions, pathways) 
+for (i, pr) in enumerate(pathway_reactions):
+    # Print general information about the pathway
+    print("Pathway i:", i)
+    for r in pr:
+        print(r)
+
