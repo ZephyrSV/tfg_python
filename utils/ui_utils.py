097db56f637a3e6d3092d8b75a16dd56e1576d3c
Zephyr Serret Verbist <zserret@me.com>
2024-01-14 14:12:48 +0100

    removed useless function

--- utils/ui_utils.py
+++ utils/ui_utils.py
@@ -8,20 +8,6 @@ def pad(x=10, y=10):
     return {'padx': x, 'pady': y}
 
 
-def gridrc(r, c, rs=1, cs=1):
-    """
-    Returns a dictionary with the grid parameters to use for tkinter widgets
-
-    Stands for grid row column.
-    :param r: row
-    :param c: column
-    :param rs: rowspan
-    :param cs: columnspan
-    :return: {'row': r, 'column': c, 'rowspan': rs, 'columnspan': cs}
-    """
-    return {'row': r, 'column': c, 'rowspan': rs, 'columnspan': cs}
-
-
 class GridUtil:
     current_row = 0
     current_column = 0
